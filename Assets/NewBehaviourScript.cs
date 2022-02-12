17712a8a4a43958882d8b7369a30cc758f05ea56
HGP47 <hgp47@thes-air.attlocal.net>
2022-02-12 12:00:24 -0800

    variables project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+ 
+    
+// Start is called before the first frame update
+    void Start()
+    {
+        string name = "Simon";
+      print("Hello, " + "my name is " + name + ". ");
+    string teamates = "Sadat, Souleymane, Khalil";
+      print("My teamates are, " + teamates + ". ");
+      string CustomizedMessage = "I'd like to let them know that";
+      print(CustomizedMessage + " I appreciate their hard work. ");
+    }
+}
